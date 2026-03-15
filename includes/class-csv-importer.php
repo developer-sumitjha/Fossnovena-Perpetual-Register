b554d7b6b5014c518b105fdc4a1453154f01ec8a
Sumit Narayan Jha <snjha@Sumits-MacBook-Air.local>
2026-03-15 11:14:12 +0545

    CSV importer fixes & update

--- includes/class-csv-importer.php
+++ includes/class-csv-importer.php
@@ -30,6 +30,7 @@ class FNPR_CSV_Importer {
                 if($import_type == "replace"){
                     $wpdb->query("TRUNCATE TABLE $table");
                 }
+                $row_count = 0;
 
                 while(($data = fgetcsv($handle, 1000, ",")) !== FALSE){
 
@@ -38,20 +39,29 @@ class FNPR_CSV_Importer {
                         continue;
                     }
 
+                    $life_stats = mb_convert_encoding($data[2], 'UTF-8', 'UTF-8, ISO-8859-1, Windows-1252');
+
+                    $life_stats = str_replace(['–','—','Ð'], '-', $life_stats);                  
+
                     $wpdb->insert($table, [
                         'entry_id' => sanitize_text_field($data[0]),
                         'entry' => sanitize_text_field($data[1]),
-                        'life_stats' => sanitize_text_field($data[2]),
+                        'life_stats' => $life_stats,
                         'sort' => sanitize_text_field($data[3]),
                     ]);
+                    $row_count++;
 
                 }
 
                 fclose($handle);
 
                 // Success notice
-                add_action('admin_notices', function() {
-                    echo '<div class="notice notice-success"><p>CSV imported successfully.</p></div>';
+                add_action('admin_notices', function() use ($row_count) {
+
+                    echo '<div class="notice notice-success is-dismissible">';
+                    echo '<p>CSV imported <strong>'.$row_count.'</strong> rows successfully.</p>';
+                    echo '</div>';
+    
                 });
                 
 
